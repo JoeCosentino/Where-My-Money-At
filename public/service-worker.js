3e2d989b093bae4f4acd0ba2fa7534dc938eb5f3
Joey Cos <joeycosentino24@gmail.com>
2022-07-03 21:37:06 -0400

    problem solving why caching isn't working

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,17 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    './index.html',
-    './js/index.js',
-    './css/styles.css',
-    '../routes/api.js'
+    "./index.html",
+    "./js/index.js",
+    "./css/styles.css",
+    "../routes/api.js"
 ];
 
 self.addEventListener('install', function(e) {
-
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
