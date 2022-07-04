dba0f21742d35f71c86ea569110c4f1c10b2334d
Joey Cos <joeycosentino24@gmail.com>
2022-07-03 21:56:30 -0400

    service workers now cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,12 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./js/index.js",
-    "./css/styles.css",
-    "../routes/api.js"
+    "/",
+    "/index.html",
+    "/js/index.js",
+    "/js/idb.js",
+    "/css/styles.css",
+    // "../routes/api.js"
 ];
 
 self.addEventListener('install', function(e) {
